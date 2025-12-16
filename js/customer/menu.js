89f59150246cf6121c4680af1bf4c6c6c1eed01e
seungaaaaa <yg19877@navercom>
2025-12-16 16:34:17 +0900

    이미지 경로수정

--- js/customer/menu.js
+++ js/customer/menu.js
@@ -44,7 +44,7 @@ async function getMenuData() {
 
                 let img = document.createElement('img')
                 img.className = 'menu-card-img'
-                img.src = menu.image
+                img.src = `./${menu.image}`
                 imgBox.appendChild(img)
 
                 let name = document.createElement('div')
@@ -90,9 +90,9 @@ getMenuData()
 
 // 메인으로 나가기
 let xBtn = document.querySelector('.menu-xBtn')
-xBtn.addEventListener('click', () => location.href = 'main.html')
+xBtn.addEventListener('click', () => location.href = 'pages/customer/main.html')
 let logo = document.querySelector('.logo')
-logo.addEventListener('click', () => location.href = 'main.html')
+logo.addEventListener('click', () => location.href = 'pages/customer/main.html')
 
 // 메뉴 목록 조회 리스트 만들기
 function sideList() {
